6a033ba788621f6e88ebb6226d05224265a0d6fc
nboldi <nboldi@elte.hu>
2016-11-07 15:15:33 +0100

    higher order case

--- Example.hs
+++ Example.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE Rank2Types, ConstraintKinds, KindSignatures, TypeFamilies, ScopedTypeVariables, MultiParamTypeClasses, AllowAmbiguousTypes
            , FlexibleContexts, FlexibleInstances, UndecidableInstances, DataKinds, TypeApplications, DeriveGeneric, DeriveDataTypeable
-           , TypeOperators, PolyKinds #-}
+           , TypeOperators, PolyKinds, ScopedTypeVariables #-}
 module Example where
 
 import GHC.Exts
@@ -13,43 +13,13 @@ import Control.Parallel.Strategies
 import Data.Type.Bool
 import Data.Type.List hiding (Distinct)
 
-type family ClassIgnoresSubtree (cls :: * -> Constraint) (typ :: *) :: Bool where
-  ClassIgnoresSubtree cls typ = Distinct (SelectedElements cls) (MemberTypes typ)
-
-type family Distinct (ls1 :: [k]) (ls2 :: [k]) :: Bool where
-  Distinct '[] ls2 = True
-  Distinct ls1 '[] = True
-  Distinct (e1 ': ls1) ls2 = Not (Find e1 ls2) && Distinct ls1 ls2
-
-type family MemberTypes (typ :: *) :: [*] where
-  MemberTypes t = GetMemberTypes '[] t
-
-type family GetMemberTypes (checked :: [*]) (typ :: *) :: [*] where 
-  -- primitive types without Rep instance
-  GetMemberTypes checked Char = '[Char]
-  GetMemberTypes checked t = GetElementTypes checked (GG.Rep t)
-
-type family GetElementTypes (checked :: [*]) (typ :: * -> *) :: [*] where 
-  GetElementTypes checked (GG.D1 md cons) = GetElementTypesCons checked cons
-
-type family GetElementTypesCons (checked :: [*]) (typ :: * -> *) where 
-  GetElementTypesCons checked (GG.C1 mc flds) = GetElementTypesFields checked flds
-  GetElementTypesCons checked (c1 GG.:+: c2) = GetElementTypesCons checked c1 `Union` GetElementTypesCons checked c2
-
-type family GetElementTypesFields (checked :: [*]) (typ :: * -> *) where 
-  GetElementTypesFields checked (fld1 GG.:*: fld2) = GetElementTypesFields checked fld1 `Union` GetElementTypesFields checked fld2
-  GetElementTypesFields checked (GG.S1 ms (GG.Rec0 t)) = GetElementTypesField checked (Find t checked) t
-  GetElementTypesFields checked (GG.U1) = '[]
-
-type family GetElementTypesField (checked :: [*]) (inChecked :: Bool) (typ :: *) where 
-  GetElementTypesField checked True typ = '[]
-  GetElementTypesField checked False typ = Insert typ (GetMemberTypes (typ ': checked) typ)
-
-type family SelectedElements (c :: * -> Constraint) :: [*]
+import ClassyPlate
+-- import TH
 
 
 -------------------------------- USAGE-SPECIFIC PART
 
+test :: A D
 test = apply @F trf $ ABC (BA (ABC B (CB B))) (CB B)
 
 class F a where
@@ -58,16 +28,16 @@ class F a where
 instance F B where
   trf b = B
 
-instance F C where
+instance F (C x) where
   trf c = CB B
   
-type instance AppSelector F A = 'False
+type instance AppSelector F (A x) = 'False
 type instance AppSelector F B = 'True
-type instance AppSelector F C = 'True
+type instance AppSelector F (C x) = 'True
 type instance AppSelector F D = 'False
 type instance AppSelector F E = 'False
 
-type instance SelectedElements F = '[ B, C ]
+-- type instance SelectedElements F = '[ B, C ]
 
 --------
 
@@ -76,20 +46,21 @@ test2 = applyM @Debug debugCs $ ABC (BA (ABC B (CB B))) (CD D)
 class Debug a where
   debugCs :: a -> IO a
 
-instance Debug C where
+instance Show x => Debug (C x) where
   debugCs c = print c >> return c
   
 type instance AppSelector Debug a = DebugSelector a
 
-type instance SelectedElements Debug = '[ C ]
+-- type instance SelectedElements Debug = '[ C ]
 
 type family DebugSelector t where
-  DebugSelector C = 'True
+  DebugSelector (C x) = 'True
   DebugSelector _ = 'False
 
 --------
 
-test3 = apply @(MonoMatch C) (monoApp (\c -> case c of CB b -> CB (BA (ABC b (CB B))); )) $ ABC (BA (ABC B (CB B))) (CB B)
+test3 :: A D
+test3 = apply @(MonoMatch (C D)) (monoApp (\c -> case c of CB b -> CB (BA (ABC b (CB B))); CD d -> CD D)) $ ABC (BA (ABC B (CB B))) (CB B)
 
 type instance SelectedElements (MonoMatch t) = '[ t ]
 
@@ -100,7 +71,7 @@ class DebugWhere a where
   debugSubtree :: a -> Bool
   debugSubtree _ = True
 
-instance DebugWhere C where
+instance Show x => DebugWhere (C x) where
   debugWhereCs c = print c >> return c
 
 instance DebugWhere D where
@@ -112,10 +83,10 @@ instance DebugWhere E where
 
 type instance AppSelector DebugWhere a = DebugWhereSelector a
 
-type instance SelectedElements DebugWhere = '[ C, D, E ]
+-- type instance SelectedElements DebugWhere = '[ forall x . C x, D, E ]
 
 type family DebugWhereSelector t where
-  DebugWhereSelector C = 'True
+  DebugWhereSelector (C x) = 'True
   DebugWhereSelector D = 'True
   DebugWhereSelector E = 'True
   DebugWhereSelector _ = 'False
@@ -125,34 +96,32 @@ test4 = applySelectiveM @DebugWhere debugWhereCs (return . debugSubtree) $ ABC (
 
 -------------------------------- REPRESENTATION-SPECIFIC PART
 
-data A = ABC B C deriving (Show, Generic, Data)
-data B = B | BA A deriving (Show, Generic, Data)
-data C = CB B | CD D deriving (Show, Generic, Data)
+data A x = ABC B (C x) deriving (Show, Generic, Data)
+data B = B | BA (A D) deriving (Show, Generic, Data)
+data C x = CB B | CD x deriving (Show, Generic, Data)
 data D = DDE D E | D deriving (Show, Generic, Data)
 data E = E deriving (Show, Generic, Data)
 
-instance NFData A
+instance NFData x => NFData (A x)
 instance NFData B
-instance NFData C
+instance NFData x => NFData (C x)
 instance NFData D
 instance NFData E
 
-type GoodOperationFor c e = (App (AppSelector c e) c e, AutoApply c (ClassIgnoresSubtree c e) e)
--- type GoodOperationFor c e = (App (AppSelector c e) c e)
+-- type GoodOperationFor c e = (App (AppSelector c e) c e, AutoApply c (ClassIgnoresSubtree c e) e)
+type GoodOperationFor c e = (App (AppSelector c e) c e)
 
-type GoodOperation c = (GoodOperationFor c A, GoodOperationFor c B, GoodOperationFor c C, GoodOperationFor c D, GoodOperationFor c E)
-
-instance GoodOperation c => Apply c A where
-  apply f (ABC b c) = app @(AppSelector c A) @c f $ ABC (apply @c f b) (apply @c f c)
-  applyM f (ABC b c) = appM @(AppSelector c A) @c f =<< (ABC <$> applyM @c f b <*> applyM @c f c)
+instance (Apply c x, GoodOperationFor c (A x), GoodOperationFor c (A D), GoodOperationFor c B, GoodOperationFor c (C x), GoodOperationFor c (C D), GoodOperationFor c D, GoodOperationFor c E) => Apply c (A x) where
+  apply f (ABC b c) = app @(AppSelector c (A x)) @c f $ ABC (apply @c f b) (apply @c f c)
+  applyM f (ABC b c) = appM @(AppSelector c (A x)) @c f =<< (ABC <$> applyM @c f b <*> applyM @c f c)
   applySelective f pred val@(ABC b c) = appIf @c f pred val (ABC (applySelective @c f pred b) (applySelective @c f pred c))
   applySelectiveM f pred val@(ABC b c) = appIfM @c f pred val (ABC <$> applySelectiveM @c f pred b <*> applySelectiveM @c f pred c)
 
-instance GoodOperation c => AutoApply c False A where
-  applyAuto f (ABC b c) = app @(AppSelector c A) @c f $ ABC (applyAuto @c @(ClassIgnoresSubtree c B) f b) (applyAuto @c @(ClassIgnoresSubtree c C) f c)
-  applyAutoM f (ABC b c) = appM @(AppSelector c A) @c f =<< (ABC <$> applyAutoM @c @(ClassIgnoresSubtree c B) f b <*> applyAutoM @c @(ClassIgnoresSubtree c C) f c)
+-- instance (GoodOperationFor c (A x), GoodOperationFor c (A D), GoodOperationFor c B, GoodOperationFor c (C x), GoodOperationFor c D, GoodOperationFor c E) => AutoApply c False (A x) where
+--   applyAuto f (ABC b c) = app @(AppSelector c A) @c f $ ABC (applyAuto @c @(ClassIgnoresSubtree c B) f b) (applyAuto @c @(ClassIgnoresSubtree c C) f c)
+--   applyAutoM f (ABC b c) = appM @(AppSelector c A) @c f =<< (ABC <$> applyAutoM @c @(ClassIgnoresSubtree c B) f b <*> applyAutoM @c @(ClassIgnoresSubtree c C) f c)
 
-instance GoodOperation c => Apply c B where
+instance (GoodOperationFor c (A D), GoodOperationFor c B, GoodOperationFor c (C D), GoodOperationFor c D, GoodOperationFor c E) => Apply c B where
   apply f B = app @(AppSelector c B) @c f B
   apply f (BA a) = app @(AppSelector c B) @c f $ BA (apply @c f a)
 
@@ -165,19 +134,19 @@ instance GoodOperation c => Apply c B where
   applySelectiveM f pred val@B = appIfM @c f pred val (return B)
   applySelectiveM f pred val@(BA a) = appIfM @c f pred val (BA <$> applySelectiveM @c f pred a)
 
-instance GoodOperation c => AutoApply c False B where
-  applyAuto f B = app @(AppSelector c B) @c f B
-  applyAuto f (BA a) = app @(AppSelector c B) @c f $ BA (applyAuto @c @(ClassIgnoresSubtree c A) f a)
+-- instance (GoodOperationFor c (A D), GoodOperationFor c B, GoodOperationFor c (C D), GoodOperationFor c D, GoodOperationFor c E) => AutoApply c False B where
+--   applyAuto f B = app @(AppSelector c B) @c f B
+--   applyAuto f (BA a) = app @(AppSelector c B) @c f $ BA (applyAuto @c @(ClassIgnoresSubtree c A) f a)
 
-  applyAutoM f B = appM @(AppSelector c B) @c f B
-  applyAutoM f (BA a) = appM @(AppSelector c B) @c f =<< (BA <$> applyAutoM @c @(ClassIgnoresSubtree c A) f a)
+--   applyAutoM f B = appM @(AppSelector c B) @c f B
+--   applyAutoM f (BA a) = appM @(AppSelector c B) @c f =<< (BA <$> applyAutoM @c @(ClassIgnoresSubtree c A) f a)
 
-instance GoodOperation c => Apply c C where
-  apply f (CB b) = app @(AppSelector c C) @c f $ CB (apply @c f b)
-  apply f (CD d) = app @(AppSelector c C) @c f $ CD (apply @c f d)
+instance (Apply c x, GoodOperationFor c (A x), GoodOperationFor c (A D), GoodOperationFor c B, GoodOperationFor c (C x), GoodOperationFor c (C D), GoodOperationFor c D, GoodOperationFor c E) => Apply c (C x) where
+  apply f (CB b) = app @(AppSelector c (C x)) @c f $ CB (apply @c f b)
+  apply f (CD d) = app @(AppSelector c (C x)) @c f $ CD (apply @c f d)
 
-  applyM f (CB b) = appM @(AppSelector c C) @c f =<< (CB <$> applyM @c f b)
-  applyM f (CD d) = appM @(AppSelector c C) @c f =<< (CD <$> applyM @c f d)
+  applyM f (CB b) = appM @(AppSelector c (C x)) @c f =<< (CB <$> applyM @c f b)
+  applyM f (CD d) = appM @(AppSelector c (C x)) @c f =<< (CD <$> applyM @c f d)
 
   applySelective f pred val@(CB b) = appIf @c f pred val (CB (applySelective @c f pred b))
   applySelective f pred val@(CD d) = appIf @c f pred val (CD (applySelective @c f pred d))
@@ -185,14 +154,14 @@ instance GoodOperation c => Apply c C where
   applySelectiveM f pred val@(CB b) = appIfM @c f pred val (CB <$> applySelectiveM @c f pred b)
   applySelectiveM f pred val@(CD d) = appIfM @c f pred val (CD <$> applySelectiveM @c f pred d)
 
-instance GoodOperation c => AutoApply c False C where
-  applyAuto f (CB b) = app @(AppSelector c C) @c f $ CB (applyAuto @c @(ClassIgnoresSubtree c B) f b)
-  applyAuto f (CD d) = app @(AppSelector c C) @c f $ CD (applyAuto @c @(ClassIgnoresSubtree c D) f d)
+-- instance (GoodOperationFor c (A x), GoodOperationFor c (A D), GoodOperationFor c B, GoodOperationFor c (C x), GoodOperationFor c D, GoodOperationFor c E) => AutoApply c False (C x) where
+--   applyAuto f (CB b) = app @(AppSelector c (C x)) @c f $ CB (applyAuto @c @(ClassIgnoresSubtree c B) f b)
+--   applyAuto f (CD d) = app @(AppSelector c (C x)) @c f $ CD (applyAuto @c @(ClassIgnoresSubtree c D) f d)
 
-  applyAutoM f (CB b) = appM @(AppSelector c C) @c f =<< (CB <$> applyAutoM @c @(ClassIgnoresSubtree c B) f b)
-  applyAutoM f (CD d) = appM @(AppSelector c C) @c f =<< (CD <$> applyAutoM @c @(ClassIgnoresSubtree c D) f d)
+--   applyAutoM f (CB b) = appM @(AppSelector c (C x)) @c f =<< (CB <$> applyAutoM @c @(ClassIgnoresSubtree c B) f b)
+--   applyAutoM f (CD d) = appM @(AppSelector c (C x)) @c f =<< (CD <$> applyAutoM @c @(ClassIgnoresSubtree c D) f d)
 
-instance GoodOperation c => Apply c D where
+instance (GoodOperationFor c E, GoodOperationFor c D) => Apply c D where
   apply f (DDE d e) = app @(AppSelector c D) @c f $ DDE (apply @c f d) (apply @c f e)
   apply f D = app @(AppSelector c D) @c f $ D
 
@@ -205,100 +174,20 @@ instance GoodOperation c => Apply c D where
   applySelectiveM f pred val@(DDE d e) = appIfM @c f pred val (DDE <$> applySelectiveM @c f pred d <*> applySelectiveM @c f pred e)
   applySelectiveM f pred val@D = appIfM @c f pred val (return D)
 
-instance GoodOperation c => AutoApply c False D where
-  applyAuto f (DDE d e) = app @(AppSelector c D) @c f $ DDE (applyAuto @c @(ClassIgnoresSubtree c D) f d) (applyAuto @c @(ClassIgnoresSubtree c E) f e)
-  applyAuto f D = app @(AppSelector c D) @c f $ D
+-- instance (GoodOperationFor c E, GoodOperationFor c D) => AutoApply c False D where
+--   applyAuto f (DDE d e) = app @(AppSelector c D) @c f $ DDE (applyAuto @c @(ClassIgnoresSubtree c D) f d) (applyAuto @c @(ClassIgnoresSubtree c E) f e)
+--   applyAuto f D = app @(AppSelector c D) @c f $ D
 
-  applyAutoM f (DDE d e) = appM @(AppSelector c D) @c f =<< (DDE <$> applyAutoM @c @(ClassIgnoresSubtree c D) f d <*> applyAutoM @c @(ClassIgnoresSubtree c E) f e)
-  applyAutoM f D = appM @(AppSelector c D) @c f =<< (return D)
+--   applyAutoM f (DDE d e) = appM @(AppSelector c D) @c f =<< (DDE <$> applyAutoM @c @(ClassIgnoresSubtree c D) f d <*> applyAutoM @c @(ClassIgnoresSubtree c E) f e)
+--   applyAutoM f D = appM @(AppSelector c D) @c f =<< (return D)
 
-instance GoodOperation c => Apply c E where
+instance (GoodOperationFor c E) => Apply c E where
   apply f E = app @(AppSelector c E) @c f E
   applyM f E = appM @(AppSelector c E) @c f E
   applySelective f pred val@E = appIf @c f pred val E
   applySelectiveM f pred val@E = appIfM @c f pred val (return E)
 
-instance GoodOperation c => AutoApply c False E where
-  applyAuto f E = app @(AppSelector c E) @c f E
-  applyAutoM f E = appM @(AppSelector c E) @c f E
+-- instance (GoodOperationFor c E) => AutoApply c False E where
+--   applyAuto f E = app @(AppSelector c E) @c f E
+--   applyAutoM f E = appM @(AppSelector c E) @c f E
 
-
---------------------------------- GENERIC PART
-
-
-type family AppSelector (c :: * -> Constraint) (a :: *) :: Bool
-
-class App (flag :: Bool) c b where
-  app :: (forall a . c a => a -> a) -> b -> b
-  appM :: Monad m => (forall a . c a => a -> m a) -> b -> m b
-  appOpt :: (forall a . c a => a -> x) -> b -> Maybe x
-
-instance c b => App 'True c b where
-  {-# INLINE app #-}
-  app f a = f a
-  {-# INLINE appM #-}
-  appM f a = f a
-  {-# INLINE appOpt #-}
-  appOpt f a = Just $ f a
-
-instance App 'False c b where
-  {-# INLINE app #-}
-  app _ a = a
-  {-# INLINE appM #-}
-  appM _ a = return a
-  {-# INLINE appOpt #-}
-  appOpt _ _ = Nothing
-
-class App (AppSelector c b) c b => Apply c b where
-  apply :: (forall a . c a => a -> a) -> b -> b
-  applyM :: Monad m => (forall a . c a => a -> m a) -> b -> m b
-
-  applySelective :: (forall a . c a => a -> a) -> (forall a . c a => a -> Bool) -> b -> b
-  applySelectiveM :: Monad m => (forall a . c a => a -> m a) -> (forall a . c a => a -> m Bool) -> b -> m b
-
-class App (AppSelector c b) c b => AutoApply c (sel :: Bool) b where
-  applyAuto :: (forall a . c a => a -> a) -> b -> b
-  applyAutoM :: Monad m => (forall a . c a => a -> m a) -> b -> m b
-
-applyAuto_ :: forall c b . AutoApply c (ClassIgnoresSubtree c b) b => (forall a . c a => a -> a) -> b -> b
-{-# INLINE applyAuto_ #-}
-applyAuto_ = applyAuto @c @(ClassIgnoresSubtree c b)
-
-applyAutoM_ :: forall c b m . (AutoApply c (ClassIgnoresSubtree c b) b, Monad m) => (forall a . c a => a -> m a) -> b -> m b
-{-# INLINE applyAutoM_ #-}
-applyAutoM_ = applyAutoM @c @(ClassIgnoresSubtree c b)
-
-instance App (AppSelector c b) c b => AutoApply c True b where
-  applyAuto f a = app @(AppSelector c b) @c f a
-  {-# INLINE applyAuto #-}
-  applyAutoM f a = appM @(AppSelector c b) @c f a
-  {-# INLINE applyAutoM #-}
-
-{-# SPECIALIZE INLINE apply :: Apply (MonoMatch x) sel b => (forall a . MonoMatch x a => a -> a) -> b -> b #-}
-
-class MonoMatch a b where
-  monoApp :: (a -> a) -> b -> b
-
-instance MonoMatch a a where
-  monoApp = id
-  {-# INLINE monoApp #-}
-
-type instance AppSelector (MonoMatch a) b = TypEq a b
-
-type family TypEq a b :: Bool where
-  TypEq a a = 'True
-  TypEq a b = 'False
-
-appIf :: forall c b . App (AppSelector c b) c b => (forall a . c a => a -> a) -> (forall a . c a => a -> Bool) -> b -> b -> b
-{-# INLINE appIf #-}
-appIf f pred val combined = app @(AppSelector c b) @c f $ case appOpt @(AppSelector c b) @c pred val of
-    Just False -> val
-    _          -> combined
-
-appIfM :: forall c b m . (App (AppSelector c b) c b, Monad m) => (forall a . c a => a -> m a) -> (forall a . c a => a -> m Bool) -> b -> m b -> m b
-{-# INLINE appIfM #-}
-appIfM f pred val combined = do
-    doChildren <- fromMaybe (return True) $ appOpt @(AppSelector c b) @c pred val
-    inner <- case doChildren of False -> return val
-                                _     -> combined
-    appM @(AppSelector c b) @c f inner
